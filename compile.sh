d537f27011c8065003ee9a0335d3688568b33d15
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2020-07-09 15:31:33 +0430

    Simplify ./compile.sh + more features

--- compile.sh
+++ compile.sh
@@ -1,44 +1,31 @@
 #!/bin/sh
 # A short script for taking "production" exports from everything
-# $1(opt): withBiber? OR file; $2(opt): withBiber? OR withNotes? $3(opt): withNotes?
+# Inputs: if you don't specify a file (/file pattern) you can start from $2
+# $1(opt): file; $2(opt): withBiber?; $3(opt): withNotes?; $4(opt): withDebug?;
 # Examples (providing that your slide.tex files are in ./src/):
 # ./compile.sh withBiber withoutNotes
 # ./compile.sh intro withBiber
-# ./compile.sh section_* withoutBiber withNotes
-
-# If the value of mode is anything other than "withoutBiber" compilation will be signifcantly slower
-arg="${1:-withoutBiber}"
+# ./compile.sh section_* withoutBiber withNotes withoutDebug
 
 slidesd="src"
 destd="build"
 header="header.tex"
-debug="withoutDebug" #| enable state is "withDebug"
 log="compile.log"
 
-cd "$slidesd"
 
-# Call this after exporting output of a command to $log file
-debug(){ [ "$debug" = "withDebug" ] && cat $log ; rm $log ;}
+compile(){ xelatex -shell-escape "$1" >> $log ;}
 
-compilenotes(){
-    sed -i '2i\\\\setbeameroption{show only notes}' "$header" # makes Beamer to export "only notes"
-    compileto "$destd/notes/"
-    sed -i '2d' "$header" # removes "only notes" marker from slides headers
-}
-
-compile(){ xelatex -shell-escape "$1" > $log ; debug ;}
-
-removeext(){ sed 's/\.tex$//' <<< "$1" ;}
+removeext(){ echo "$1" | sed 's/\.tex$//' ;}
 
 compileto(){ # $1: Destination folder for pdf; $2(opt): file; $3(opt): withBiber?;
-    pattern="${2:-*}.tex"
+    pattern="$2.tex"
     find .\
         -name "$pattern"\
         ! -name 'header.tex'\
-        -print0 | while IFS= read -r -d '' file; do
+        -print | while IFS= read -r file; do
             compile "$file"
             [ "$3" = "withBiber" ] && {
-                biber "$(removeext "$file")" > $log ; debug
+                biber "$(removeext "$file")" >> $log
                 echo "bibed $file"
             }
             compile "$file"
@@ -47,30 +34,35 @@ compileto(){ # $1: Destination folder for pdf; $2(opt): file; $3(opt): withBiber
 
     # Move PDFs to the target folder
     mkdir "../$1" >/dev/null 2>&1
+    # shellcheck disable=SC2035
     mv -v *.pdf "../$1"
     echo "moved pdfs to $1"
-
-    # Remove auto-generated files
-    find . -maxdepth 1 -type f\
-        ! -name '*.tex'\
-        ! -name '*.sh'\
-        ! -name '*.bib'\
-        ! -name '*.org'\
-        ! -name '*.md'\
-        ! -name '.gitignore'\
-        ! -name 'README'\
-        ! -name 'LICENSE'\
-        -exec rm -f {} +
-    echo "removed auxilary files"
 }
 
 
-if [ "$arg" = "withoutBiber" ] ;then
-    compileto "$destd/"
-elif [ "$arg" = "withBiber" ] ;then
-    compileto "$destd/" '*' withBiber
-    [ "$2" = "withNotes" ] && compilenotes # BEAMER ONLY: Prepare to only compile notes
+cd "$slidesd" || exit 1
+if [ "$1" = "withBiber" ] ;then
+    compileto "$destd/" '*' "$1"
 else
-    compileto "$destd/" "$(removeext "$file")" "$2"
-    [ "$2" = "withNotes" ] && compilenotes # BEAMER ONLY: Prepare to only compile notes
+    compileto "$destd/" "$(removeext "$1")" "$2"
+    shift
 fi
+# BEAMER ONLY: Prepare to only compile notes
+[ "$2" = "withNotes" ] && {
+    sed -i '2i\\\\setbeameroption{show only notes}' "$header" # makes Beamer to export "only notes"
+    compileto "$destd/notes/"
+    sed -i '2d' "$header" # removes "only notes" marker from slides headers
+}
+[ "$3" = "withDebug" ] && cat $log
+# Remove auto-generated files
+find . -maxdepth 1 -type f\
+    ! -name '*.tex'\
+    ! -name '*.sh'\
+    ! -name '*.bib'\
+    ! -name '*.org'\
+    ! -name '*.md'\
+    ! -name '.gitignore'\
+    ! -name 'README'\
+    ! -name 'LICENSE'\
+    -exec rm -f {} +
+echo "removed auxilary files"
