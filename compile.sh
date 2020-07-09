a4d3b428ee6a8f50b0a718444b75a0c9c203cf02
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2020-07-09 20:51:05 +0430

    Rewrite compile.sh

--- compile.sh
+++ compile.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
-# A short script for taking "production" exports from everything
-# Inputs: if you don't specify a file (/file pattern) you can start from $2
-# $1(opt): file; $2(opt): withBiber?; $3(opt): withNotes?; $4(opt): withDebug?;
-# Examples (providing that your slide.tex files are in ./src/):
-# ./compile.sh withBiber withoutNotes
-# ./compile.sh intro withBiber
-# ./compile.sh section_* withoutBiber withNotes withoutDebug
+# TODO write a proper description
 
-slidesd="src"
-destd="build"
-header="header.tex"
-log="compile.log"
-
-
-compile(){ xelatex -shell-escape "$1" >> $log ;}
+# -----
+# BEGIN Header code
+# -----
+checkin(){ # Checks paramater and respond. $2 is the flag and $1 is value.
+    # shellcheck disable=SC2015
+    [ -n "$1" ] && return 0 || { [ -n "$2" ] && error "No parameter for $2" || error "Bad inputs! Seek help." ;}
+}
 
 removeext(){ echo "$1" | sed 's/\.tex$//' ;}
+
+cleanup(){ # Remove auto-generated files
+    find . -maxdepth 1 -type f\
+        ! -name '*.tex'\
+        ! -name '*.sh'\
+        ! -name '*.bib'\
+        ! -name '*.org'\
+        ! -name '*.md'\
+        ! -name '.gitignore'\
+        ! -name 'README'\
+        ! -name 'LICENSE'\
+        -exec rm -f {} +
+    echo "removed auxilary files"
+}
+
+compile(){ xelatex -shell-escape "$1" >> $log ;}
 
 compileto(){ # $1: Destination folder for pdf; $2(opt): file; $3(opt): withBiber?;
     pattern="$2.tex"
@@ -39,30 +49,56 @@ compileto(){ # $1: Destination folder for pdf; $2(opt): file; $3(opt): withBiber
     echo "moved pdfs to $1"
 }
 
-
-cd "$slidesd" || exit 1
-if [ "$1" = "withBiber" ] ;then
-    compileto "$destd/" '*' "$1"
-else
-    compileto "$destd/" "$(removeext "$1")" "$2"
-    shift
-fi
-# BEAMER ONLY: Prepare to only compile notes
-[ "$2" = "withNotes" ] && {
+compilenotes(){ # BEAMER ONLY: Prepare to only compile notes
     sed -i '2i\\\\setbeameroption{show only notes}' "$header" # makes Beamer to export "only notes"
-    compileto "$destd/notes/"
+    compileto "$1" "$2" "$3"
     sed -i '2d' "$header" # removes "only notes" marker from slides headers
 }
-[ "$3" = "withDebug" ] && cat $log
-# Remove auto-generated files
-find . -maxdepth 1 -type f\
-    ! -name '*.tex'\
-    ! -name '*.sh'\
-    ! -name '*.bib'\
-    ! -name '*.org'\
-    ! -name '*.md'\
-    ! -name '.gitignore'\
-    ! -name 'README'\
-    ! -name 'LICENSE'\
-    -exec rm -f {} +
-echo "removed auxilary files"
+
+
+# Config (not available via input)
+header="header.tex"
+log="compile.log"
+helpmsg="" # TODO Write a help message
+# Default values
+destd="build"
+slidesd="src"
+notesd="$destd/notes"
+debug="withoutDebug"
+biber="withoutBiber"
+notes="withoutNotes"
+keep="keepAuxFiles"
+filepattern="*"
+
+
+# Inputs and flag management
+[ -n "$1" ] && { echo "$1" | grep -q "^-" || { input=$1 && shift ;} ;} #If the first input wasn't a flag read it as the $file pattern
+while true; do
+    case "$1" in
+        -f | --file | -i | --input) checkin "$2" "$1" && filepattern="$(removeext "$2")" && shift 2 ;;
+        -d | --debug) debug="withDebug" && shift ;;
+        -b | --biber) biber="withBiber" && shift ;;
+        -n | --notes) notes="withNotes" && shift ;;
+        -o | --only-notes) notes="onlyNotes" && shift ;;
+        -c | --destination-dir) checkin "$2" "$1" && destd="$2" && shift 2 ;;
+        -s | --slides-dir) checkin "$2" "$1" && slidesd="$2" && shift 2 ;;
+        -y | --notesd-dir) checkin "$2" "$1" && notesd="$2" && shift 2 ;;
+        -k | --keep) keep="keepAuxFiles" && shift ;;
+        -c | --clean) keep="removeAuxFiles" && shift ;;
+        -q | --quite) debug="withoutDebug" && shift ;;
+        -h | --help) echo "$helpmsg" && exit 0 ;;
+        -*) echo "Invalid option: $1" && echo "$helpmsg" && exit 1 ;;
+        *)  break ;; # No more options
+    esac
+done
+# -----
+# END Header code
+# -----
+
+
+cd "$slidesd" || exit 1
+[ "$notes" = "onlyNotes" ] || compileto "$destd/" "$filepattern" "$biber"
+[ "$notes" != "withoutNotes" ] && compilenotes "$notesd/" "$filepattern" "$biber"
+[ "$debug" = "withDebug" ] && cat $log
+[ "$keep" != "keepAuxFiles" ] && cleanup
+exit 0
